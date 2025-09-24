78d31629327573992cacb7baea550f20392020b2
ilcann <ilcan21@itu.edu.tr>
2025-09-24 23:56:50 +0300

    fix: simplify API URL to use a static path

--- src/lib/api/base.ts
+++ src/lib/api/base.ts
@@ -1,9 +1,7 @@
 import type { ApiResponse } from '@/lib/types/api-response';
 
 
-const API_HOST = import.meta.env.VITE_HOST ?? 'localhost';
-const API_PORT = import.meta.env.VITE_PORT ?? 3000;
-export const apiUrl = `http://${API_HOST}:${API_PORT}/api`;
+export const apiUrl = `/api`;
 
 export async function apiFetch<T>(path: string, options?: RequestInit): Promise<ApiResponse<T>> {
   const res = await fetch(`${apiUrl}${path}`, options);
